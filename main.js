0b0cd5bcf72856d4375adcc958dbcb54725e381c
Muhammad Ahmed <96483397+Aplha-pro@users.noreply.github.com>
2025-01-28 16:28:23 +0500

    added

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-document.getElementById("show-more-btn").addEventListener("click", function () {
-    const hiddenBlogs = document.querySelector(".blog-hidden");
-    hiddenBlogs.style.display = hiddenBlogs.style.display === "grid" ? "none" : "grid";
-    this.textContent = hiddenBlogs.style.display === "grid" ? "Show Less" : "Show More";
+// document.getElementById("show-more-btn").addEventListener("click", function () {
+//     const hiddenBlogs = document.querySelector(".blog-hidden");
+//     hiddenBlogs.style.display = hiddenBlogs.style.display === "grid" ? "none" : "grid";
+//     this.textContent = hiddenBlogs.style.display === "grid" ? "Show Less" : "Show More";
+// });
+
+document.getElementById("show-more-btn").addEventListener("click", function() {
+    var hiddenBlogs = document.querySelector(".hidden-blogs");
+    if (hiddenBlogs.style.display === "none" || hiddenBlogs.style.display === "") {
+        hiddenBlogs.style.display = "block";
+        this.textContent = "Show Less"; // Change button text
+    } else {
+        hiddenBlogs.style.display = "none";
+        this.textContent = "Show More"; // Reset button text
+    }
 });
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const hiddenBlogs = document.querySelector(".blog-hidden");
